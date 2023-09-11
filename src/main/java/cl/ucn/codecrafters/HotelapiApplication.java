7b92cfcb1265fc15b10571e196d3412ea2cebfd2
Marcelo Céspedes <m.cespedes.arq@gmail.com>
2023-09-11 17:56:04 -0300

    first commit

---
+++ src/main/java/cl/ucn/codecrafters/HotelapiApplication.java
@@ -0,0 +1,13 @@
+package cl.ucn.codecrafters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelapiApplication.class, args);
+	}
+
+}
