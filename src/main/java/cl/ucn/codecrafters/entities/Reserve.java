83fc52ea61322b8e954cd4558b6a18f272d1220d
2kSebaNG <sebastian.nunez03@alumnos.ucn.cl>
2023-10-15 15:28:47 -0300

    feat: Reserve's CRUD basic functions finished

--- src/main/java/cl/ucn/codecrafters/entities/Reserve.java
+++ src/main/java/cl/ucn/codecrafters/entities/Reserve.java
@@ -1,11 +1,19 @@
 package cl.ucn.codecrafters.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.sql.Time;
 import java.time.LocalDateTime;
 import java.util.Date;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "reserve")
 public class Reserve extends Base{
